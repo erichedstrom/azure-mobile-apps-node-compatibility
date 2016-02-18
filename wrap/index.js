6d0ad8c532a15ecaa8587a6778a078e462bb3aab
danderson00 <danderson00@gmail.com>
2016-02-18 16:15:32 +1100

    Started wrappers

--- wrap/index.js
+++ wrap/index.js
@@ -1,6 +1,11 @@
+var request = require('./request'),
+    user = require('./user')
+
 module.exports = {
     read: function (handler) {
-        
+        return function (context) {
+            handler(context.query, user(context.user), request(context.request))
+        }
     },
     insert: function (handler) {
 
@@ -12,3 +17,15 @@ module.exports = {
 
     }
 }
+
+function createRequestWrapper(context, handler) {
+    
+}
+
+/*
+function insert(item, user, request) {
+function update(item, user, request) {
+function del(id, user, request) {
+function read(query, user, request) {
+
+*/
