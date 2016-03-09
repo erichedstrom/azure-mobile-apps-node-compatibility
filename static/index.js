ded4419b980e7a76ad01b41dd2605a701b1d2204
Dale Anderson <danderson00@gmail.com>
2016-03-09 00:39:02 -0800

    Safer handling of generated gitignore

--- static/index.js
+++ static/index.js
@@ -2,7 +2,11 @@ var fs = require('fs'),
     path = require('path')
 
 module.exports = fs.readdirSync(__dirname).reduce(function (files, filename) {
+    // npm renames .gitignore to .npmignore - safer to treat specially
+    var targetFilename = filename === 'gitignore' ? '.gitignore' : filename;
+
     if(filename !== 'index.js')
-        files[filename] = fs.readFileSync(path.join(__dirname, filename))
+        files[targetFilename] = fs.readFileSync(path.join(__dirname, filename))
+
     return files
 }, { })
