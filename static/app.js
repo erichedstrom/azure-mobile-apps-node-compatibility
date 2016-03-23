25eda809c4fbc443cd5a27ccd39e5b94ac0cd65b
Dale Anderson <dalander@microsoft.com>
2016-03-23 13:18:21 -0700

    Added telemetry header

--- static/app.js
+++ static/app.js
@@ -1,7 +1,9 @@
 var app = require('express')(),
-    mobileApp = require('azure-mobile-apps')()
+    mobileApp = require('azure-mobile-apps')(),
+    telemetry = require('./telemetry')
 
 mobileApp.tables.import('tables')
 mobileApp.api.import('api')
+mobileApp.use(telemetry)
 app.use(mobileApp)
 app.listen(process.env.PORT || 3000)
